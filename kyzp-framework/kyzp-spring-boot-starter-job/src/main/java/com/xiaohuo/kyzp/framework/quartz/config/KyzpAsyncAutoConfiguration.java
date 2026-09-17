986991f090658ee6cd737d49215a970d329a35a5
chenbo <443756309@qq.com>
2026-09-17 16:38:16 +0800

    11

--- kyzp-framework/kyzp-spring-boot-starter-job/src/main/java/com/xiaohuo/kyzp/framework/quartz/config/KyzpAsyncAutoConfiguration.java
+++ kyzp-framework/kyzp-spring-boot-starter-job/src/main/java/com/xiaohuo/kyzp/framework/quartz/config/KyzpAsyncAutoConfiguration.java
@@ -17,7 +17,7 @@ import org.springframework.core.task.SimpleAsyncTaskExecutor;
 public class KyzpAsyncAutoConfiguration {
 
     @Bean
-    public BeanPostProcessor threadPoolTaskExecutorBeanPostProcessor() {
+    public static BeanPostProcessor threadPoolTaskExecutorBeanPostProcessor() {
         return new BeanPostProcessor() {
 
             @Override
